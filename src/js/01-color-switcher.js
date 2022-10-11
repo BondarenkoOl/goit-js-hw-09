b4fb0aa67a05821e67168fee5cea68b3ca15274e
BondarenkoOl <bondarenko.ptl@gmail.com>
2022-10-11 03:14:47 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,10 +6,11 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+stopBtn.setAttribute('disabled', true);
+let bgColor = null;
+
 startBtn.addEventListener('click', changeBodyColor);
 stopBtn.addEventListener('click', stopChangeBodyColor);
-
-stopBtn.setAttribute('disabled', true);
 
 function changeBodyColor() {
   startBtn.setAttribute('disabled', true);
